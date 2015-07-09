b7dc27803bf9f43351c62fb573a981a11308bcd7
Katelyn <godfrey.katelyn@gmail.com>
2015-07-09 08:11:24 -0400

    Added Icons and App Opitmization

--- Assets/Scripts/PlayVideo.cs
+++ Assets/Scripts/PlayVideo.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class PlayVideo : MonoBehaviour {
 
 	public string movieName;
-	public MovieTexture movie;
 	private float movieTimer;
 	private bool videoPlay = false;
 
@@ -14,31 +13,13 @@ public class PlayVideo : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetMouseButtonDown(0) && movie.isPlaying)
-		{
-			movie.Stop();
-			Debug.Log ("STOP");
-			videoPlay = false;
-		}
 	}
 
 	public void PlayM (){
 
 		if(Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer){
-#if !UNITY_WEBPLAYER
 			Handheld.PlayFullScreenMovie (movieName, Color.black, FullScreenMovieControlMode.CancelOnInput, FullScreenMovieScalingMode.AspectFit);
-#endif
-		}
-		else{
-			Debug.Log ("PLAY VIDEO");	
-			videoPlay = true;
 		}
 	}
 
-	void OnGUI(){
-		if(videoPlay){
-			GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),movie);
-			movie.Play ();
-		}
-	}
 }
