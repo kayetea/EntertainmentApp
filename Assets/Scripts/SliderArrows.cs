d58e4abb21761abeaaa7a2f63b099505bf775abb
Katelyn <godfrey.katelyn@gmail.com>
2015-07-17 13:54:37 -0400

    Slider Bar Arrows

---
+++ Assets/Scripts/SliderArrows.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderArrows : MonoBehaviour {
+
+	public Scrollbar scrollTarget;
+	public Button otherArrow;
+	public float Step = 0.1f;
+
+	void Start()
+	{
+		scrollTarget.value = 0;
+	}
+
+	public void Increment()
+	{
+		scrollTarget.value = Mathf.Clamp(scrollTarget.value + Step, 0, 1);
+		GetComponent<Button>().interactable = scrollTarget.value != 1;
+		otherArrow.interactable = true;
+	}
+	
+	public void Decrement()
+	{
+		scrollTarget.value = Mathf.Clamp(scrollTarget.value - Step, 0, 1);
+		GetComponent<Button>().interactable = scrollTarget.value != 0;
+		otherArrow.interactable = true;
+	}
+
+}
