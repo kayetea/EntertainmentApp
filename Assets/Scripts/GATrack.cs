80b0c6bb0f73abcec9800d38fe2b47a106d2273c
Katelyn <godfrey.katelyn@gmail.com>
2016-03-30 15:45:20 -0400

    ga tracking c# file added

---
+++ Assets/Scripts/GATrack.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GATrack : MonoBehaviour {
+
+	public GoogleAnalyticsV4 googleAnalytics;
+
+	void Start(){
+		googleAnalytics.StartSession ();
+	}
+}
